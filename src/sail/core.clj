622e2281de11049cf46136dcdc917d4533f23c99
John Berthels <john@berthels.co.uk>
2016-09-27 18:18:26 +0100

    draw a (fixed for now) vector field on top of the scalar field

--- src/sail/core.clj
+++ src/sail/core.clj
@@ -8,12 +8,12 @@
 
 (def pixels-per-metre 1)
 
-(def ticks-per-sec 20)
+(def ticks-per-sec 10)
+(def num-cells 51)
 
 ;; Our arena is a world with the origin at the centre
 (def width (/ screen-width pixels-per-metre 2))
 (def height (/ screen-height pixels-per-metre 2))
-(def num-cells 51)
 
 (defn irange->x
   [i]
@@ -53,6 +53,10 @@
        (math/sqrt (+ (* x x)
                      (* y y))))))
 
+(defn vector-field
+  [[x y] tick]
+  [5 5])
+
 (defn setup []
   (q/frame-rate ticks-per-sec)
   (let [field-vals (map #(scalar-field % 0) (cell-pos))]
@@ -83,26 +87,69 @@
           w
           h))
 
-(defn draw-field-pos
-  [[min-val max-val] f pos tick]
-  (let [[x y] (pos->screen pos)
-        box-width (/ screen-width num-cells)
-        box-height (/ screen-width num-cells)
-        current-val (f pos tick)]
-    ;    (q/ellipse x y 5 5)))
-    (q/with-fill (field->colour min-val max-val current-val)
-      (centered-rect x y box-width box-height))
-    current-val))
+(defn vec-scale
+  [{x :x y :y :as px} scale]
+  (if (nil? px)
+    {:x 0 :y 0}
+    (conj px {:x (* x scale)
+              :y (* y scale)})))
+
+(defn vec-add
+  ([px] px)
+  ([px py]
+  (if (nil? py)
+    px
+    (if (nil? px)
+      {:x 0 :y 0}
+      (conj px {:x (+ (:x px) (:x py))
+                :y (+ (:y px) (:y py))})))))
+
+(defn vec-subtract
+  [px py]
+  (vec-add px (vec-scale py -1)))
+
+(defn vec-length
+  [px]
+  (if (nil? px)
+    0
+    (math/sqrt (+ (* (:x px) (:x px))
+                  (* (:y px) (:y px))))))
+
+(defn vec-normalise
+  [{x :x y :y :as px}]
+  (let [length (vec-length px())]
+    (if (= length 0)
+      {:x 0 :y 0}
+      (vec-scale px (/ 1 length)))))
+
+(defn quil-draw-vec
+  [cx cy [vx vy]]
+  (q/line cx cy (+ cx vx) (+ cy vy)))
+
+(defn draw-vector-field-at-pos
+  [[x y] vval]
+  (q/with-stroke [255 0 0]
+    (quil-draw-vec x y vval)))
+
+(defn draw-scalar-field-at-pos
+  [[x y] sval [min-val max-val]]
+  (let [box-width (/ screen-width num-cells)
+        box-height (/ screen-width num-cells)]
+    (q/with-fill (field->colour min-val max-val sval)
+      (centered-rect x y box-width box-height))))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   (let [range-ref (:range-ref state)]
     (q/background 240)
     (q/stroke 0 0 0)
-    (let [new-vals (map #(draw-field-pos (deref range-ref)
-                                         scalar-field
-                                         %
-                                         (:tick state))
+    (let [tick (:tick state)
+          new-vals (map #(let [sval (scalar-field % tick)
+                               vval (vector-field % tick)
+                               screen-pos (pos->screen %)]
+                           (draw-scalar-field-at-pos screen-pos sval (deref range-ref))
+                           (draw-vector-field-at-pos screen-pos vval)
+                           sval)
                         (cell-pos))]
       (swap! range-ref (fn [old-val] [(apply min new-vals) (apply max new-vals)])))))
 
