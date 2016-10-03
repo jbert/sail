41c9ab6cbeb51d98c6e977c9b1dee67d9ca2f132
John Berthels <john@berthels.co.uk>
2016-10-03 08:20:53 +0100

    update drawing code to use vecs

--- src/sail/core.clj
+++ src/sail/core.clj
@@ -9,8 +9,8 @@
 
 (def pixels-per-metre 1)
 
-(def ticks-per-sec 10)
-(def num-cells 51)
+(def ticks-per-sec 1)
+(def num-cells 21)
 
 ;; Our arena is a world with the origin at the centre
 (def width (/ screen-width pixels-per-metre 2))
@@ -33,17 +33,17 @@
   (let [half (/ num-cells 2)]
     (for [i (range (- half) half)
           j (range (- half) half)]
-      [(irange->x i) (irange->y j)])))
+      (v/make-vec (irange->x i) (irange->y j)))))
 
-(defn safe-atan
-  [x y]
-  (let [epsilon 1e-9]
-    (if (< (Math/abs x) epsilon)
-      (let [pi2 (/ Math/PI 2)]
-        (if (pos? y)
-          pi2
-          (- pi2)))
-      (Math/atan (/ y x)))))
+;(defn safe-atan
+;  [x y]
+;  (let [epsilon 1e-9]
+;    (if (< (Math/abs x) epsilon)
+;      (let [pi2 (/ Math/PI 2)]
+;        (if (pos? y)
+;          pi2
+;          (- pi2)))
+;      (Math/atan (/ y x)))))
 
 ;(defn scalar-field
 ;  [tick [x y]]
@@ -51,12 +51,13 @@
 ;    (+ v (Math/sin (/ v 2)))))
 
 (defn scalar-field
-  [tick [x y]]
-  (let [theta (* Math/PI 2 (/ x width))
-        gamma (* Math/PI 2 (/ y height))
-        phi (/ (* Math/PI 2 (/ tick ticks-per-sec)) 5)]
-    (+ (Math/sin (+ phi (* 3 (+ theta))))
-       (Math/sin (+ phi (* 3 (+ gamma)))))))
+  [tick pos]
+  (let [{x :x y :y} (v/to-cart pos)]
+    (let [theta (* Math/PI 2 (/ x width))
+          gamma (* Math/PI 2 (/ y height))
+          phi (/ (* Math/PI 2 (/ tick ticks-per-sec)) 5)]
+      (+ (Math/sin (+ phi (* 3 (+ theta))))
+         (Math/sin (+ phi (* 3 (+ gamma))))))))
 
 ;(defn scalar-field
 ;  [tick [x y]]
@@ -70,7 +71,7 @@
 (defn make-boat
   [name x y]
   {:name name
-   :pos [x y]})
+   :pos (v/make-vec x y)})
 
 (defn setup []
   (q/frame-rate ticks-per-sec)
@@ -84,9 +85,10 @@
   (update state :tick inc))
 
 (defn pos->screen
-  [[x y]]
-  [(* screen-width (+ (/ x width) 0.5))
-   (* screen-height (+ (/ y height) 0.5))])
+  [pos]
+  (let [{x :x y :y} (v/to-cart pos)]
+    (v/make-vec (* screen-width (+ (/ x width) 0.5))
+                (* screen-height (+ (/ y height) 0.5)))))
 
 (defn field->colour
   [minv maxv v]
@@ -106,7 +108,9 @@
 (defn vector-derivative
   [tick sf pos]
   (let [step 1
-        sample-points (map #(v/scale % step) [[1 0] [0 1] [-1 0] [0 -1]])
+        sample-points (map (fn [[x y]] (v/scale (v/make-vec x y)
+                                                step))
+                           [[1 0] [0 1] [-1 0] [0 -1]])
         sval (sf tick pos)
         deltas (map #(v/scale % (/ (- (sf tick (v/add pos %))
                                       sval)
@@ -119,22 +123,26 @@
 ;  [5 5])
 
 (defn quil-draw-vec
-  [cx cy [vx vy]]
-  (q/ellipse cx cy 2 2)
-  (q/line cx cy (+ cx vx) (+ cy vy)))
+  [pos v]
+  (let [{x :x y :y} (v/to-cart pos)
+        {vx :x vy :y} (v/to-cart v)]
+    (q/ellipse x y 2 2)
+    (q/line x y (+ x vx) (+ y vy))))
 
 (defn draw-vector-field-at-pos
-  [[x y] vval]
+  [pos vval]
   (q/with-stroke [255 0 0]
     (q/with-fill [255 0 0]
-      (quil-draw-vec x y (v/scale vval 100)))))
+      (quil-draw-vec pos (v/scale vval 100)))))
 
 (defn draw-scalar-field-at-pos
-  [[x y] sval [min-val max-val]]
-  (let [box-width (/ screen-width num-cells)
-        box-height (/ screen-width num-cells)]
-    (q/with-fill (field->colour min-val max-val sval)
-      (centered-rect x y box-width box-height))))
+  [pos sval [min-val max-val]]
+;  (println "JB" pos sval min-val max-val)
+  (let [{x :x y :y} (v/to-cart pos)]
+    (let [box-width (/ screen-width num-cells)
+          box-height (/ screen-width num-cells)]
+      (q/with-fill (field->colour min-val max-val sval)
+        (centered-rect x y box-width box-height)))))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
@@ -142,23 +150,23 @@
     (q/background 240)
     (q/stroke 0 0 0)
     (let [tick (:tick state)
-;          new-vals (map #(let [sval (scalar-field tick %)
-;                               vval (vector-derivative tick scalar-field %)
-;                               screen-pos (pos->screen %)]
-;                           (draw-scalar-field-at-pos screen-pos sval (deref range-ref))
-;                           (draw-vector-field-at-pos screen-pos vval)
-;                           sval)
-;                        (cell-pos))]
-;      (swap! range-ref (fn [old-val] [(apply min new-vals) (apply max new-vals)])))))
-          ])))
+          new-vals (map #(let [sval (scalar-field tick %)
+                               vval (vector-derivative tick scalar-field %)
+                               screen-pos (pos->screen %)]
+                           (draw-scalar-field-at-pos screen-pos sval (deref range-ref))
+                           (draw-vector-field-at-pos screen-pos vval)
+                           sval)
+                        (cell-pos))]
+      (swap! range-ref (fn [old-val] [(apply min new-vals) (apply max new-vals)])))))
+  ;        ])))
 
-;(q/defsketch sail
-;  :title "You spin my circle right round"
-;  :size [screen-width screen-height]
-;  :setup setup
-;  :update update-state
-;  :draw draw-state
-;  :middleware [m/fun-mode])
+(q/defsketch sail
+  :title "You spin my circle right round"
+  :size [screen-width screen-height]
+  :setup setup
+  :update update-state
+  :draw draw-state
+  :middleware [m/fun-mode])
 
 
 (defn -main
